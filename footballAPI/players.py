fdd8338c8a52a45d7e3618092f286e60a031215b
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-16 00:26:09 +0100

    Update players.py

--- footballAPI/players.py
+++ footballAPI/players.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
 
-from .core import *
+# from .core import *
 
 class Players(object):
 
     def __init__(self, *args):
+        pass
+
+    def json_players(self):
         pass
