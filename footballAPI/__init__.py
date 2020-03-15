a9dfd9d859e5e6a513f0d0f97d01b6096773353d
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-16 00:25:57 +0100

    Update __init__.py

--- footballAPI/__init__.py
+++ footballAPI/__init__.py
@@ -15,14 +15,32 @@ from .leagues import Leagues
 
 
 class FootballAPI(Players, Teams, Leagues):
+
     def __init__(self, *args):
         Players.__init__(self, *args)
         Teams.__init__(self, *args)
         Leagues.__init__(self, *args)
+        self.json_data = None
+
+    def set_parameters(self, parameters_dictionary):
+        self.parameters_dictionary = parameters_dictionary
+        self.set_json_data()
+
+    def set_json_data(self):
+
+        if is_players_request(self.parameters_dictionary):
+            self.json_data = self.json_players()
+
+        elif is_teams_request(self.parameters_dictionary):
+            self.json_data = self.json_teams()
+
+        elif is_leagues_request(self.parameters_dictionary):
+            self.json_data = self.json_leagues()
+
 
 __copyright__ = "Copyright 2016 by [entrez vos pseudos github] paul-mathieu"
 __authors__ = ["paul-mathieu", "[entrez vos pseudos github]"]
 __source__ = "https://github.com/paul-mathieu/English-football-leagues-API"
 __license__ = "MIT"
 
-__all__ = ["Football", "API",]
+__all__ = ["Football", "API", ]
