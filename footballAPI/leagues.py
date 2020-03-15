bdc1512d45d1ff710d75b5a10ddc0547dbcc9650
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-16 00:26:03 +0100

    Update leagues.py

--- footballAPI/leagues.py
+++ footballAPI/leagues.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
 
-from .core import *
+# from .core import *
 
 class Leagues(object):
 
     def __init__(self, *args):
+        pass
+
+    def json_leagues(self):
         pass
