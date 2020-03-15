9a0c588d840c442d5a8cf7bb50576136bf04da67
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-16 00:26:01 +0100

    Update core.py

---
+++ footballAPI/core.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+import json
+import requests
+import datetime
+
+BASE_URL = "https://uk.soccerway.com"
+
+PLAYERS_URL = None
+
+TEAMS_START_URL = None
+TEAMS_END_URL = None
+
+LEAGUES_URL = None
+
+THIS_YEAR = int(datetime.datetime.now()[:4])
+
+
+def get_HTML(url, convert_to_json=False):
+    html_content = requests.get(url).text
+    if not convert_to_json:
+        return html_content
+    else:
+        return json.loads(json.dumps(html_content))
+
+
+def is_players_request(parameters_dictionary):
+    if "API type" not in parameters_dictionary.keys():
+        return False
+    return parameters_dictionary["API type"] in ["players", "Players", "PLAYERS"]
+
+
+def is_teams_request(parameters_dictionary):
+    if "API type" not in parameters_dictionary.keys():
+        return False
+    return parameters_dictionary["API type"] in ["teams", "Teams", "TEAMS"]
+
+
+def is_leagues_request(parameters_dictionary):
+    if "API type" not in parameters_dictionary.keys():
+        return False
+    return parameters_dictionary["API type"] in ["leagues", "Leagues", "LEAGUES"]
