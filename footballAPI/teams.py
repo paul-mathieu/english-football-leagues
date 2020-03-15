dbec0561a8f9c777d48f120d9ce42347b70cc912
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-16 00:26:13 +0100

    Update teams.py

--- footballAPI/teams.py
+++ footballAPI/teams.py
@@ -1,8 +1,16 @@
 # -*- coding: UTF-8 -*-
 
-from .core import *
+# from .core import *
 
 class Teams(object):
 
+    """
+    utl type : https://uk.soccerway.com/national/england/league-one/20182019/regular-season/r48115/tables/
+      -> https://uk.soccerway.com/national/[country]/[League]/[yearyear]/regular-season/tables
+    """
+
     def __init__(self, *args):
+        pass
+
+    def json_teams(self):
         pass
