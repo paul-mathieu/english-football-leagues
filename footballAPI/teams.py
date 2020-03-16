d88e95d16cdeb97607853095f4be856ff8441093
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-16 10:39:34 +0100

    Update teams.py

--- footballAPI/teams.py
+++ footballAPI/teams.py
@@ -10,28 +10,48 @@ class Teams(object):
       -> https://uk.soccerway.com/national/[country]/[League]/[yearyear]/regular-season/tables
     """
 
+    # ===============================================================
+    #   Initialisation
+    # ===============================================================
+
     def __init__(self, *args):
         self.parameters_dictionary = None
         self.URL = None
         # print('init')
-
-    def json_teams(self, parameters_dictionary):
-        self.set_parameters_dictionary(parameters_dictionary)
-        self.set_URL()
-        self.display()
+        
+    # ===============================================================
+    #   Setters
+    # ===============================================================
 
     def set_parameters_dictionary(self, parameters_dictionary):
+        """
+        Set the query option dictionary
+            :param parameters_dictionary: dictionary of values
+            :type parameters_dictionary: dict
+        """
         self.parameters_dictionary = parameters_dictionary
         # print("test")
 
-    def set_URL(self):
+    def set_URL_teams(self):
+        """
+        Set the URL for the request
+        """
         self.URL = BASE_URL + TEAMS_START_URL + \
                    "/" + self.parameters_dictionary["country"] + \
                    "/" + self.parameters_dictionary["league"] + \
                    "/" + self.get_year() + \
                    TEAMS_END_URL
 
+    # ===============================================================
+    #   Getters
+    # ===============================================================
+
     def get_year(self):
+        """
+        Returns the year as a season
+            :return: two years pasted (ex: 20192020)
+            :rtype: str
+        """
         if "start year" in self.parameters_dictionary.keys():
             year = int(self.parameters_dictionary["start year"])
             return str(year) + str(year + 1)
@@ -40,6 +60,26 @@ class Teams(object):
             return str(year - 1) + str(year)
         else:
             return str(THIS_YEAR - 1) + str(THIS_YEAR)
+
+    # ===============================================================
+    #   Methods
+    # ===============================================================
+
+    def json_teams(self, parameters_dictionary):
+        """
+        Apply the query with parameters and return a dictionary (json)
+            :param parameters_dictionary: dictionary of query parameters
+            :type parameters_dictionary: dict
+            :return json_data: json data
+            :rtype json_data: dict
+        """
+        self.set_parameters_dictionary(parameters_dictionary)
+        self.set_URL_teams()
+        self.display()
+
+# ===============================================================
+#   Prints and debugs
+# ===============================================================
 
     def display(self):
         print(get_HTML(self.URL))
