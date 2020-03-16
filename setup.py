d55dbee3df8cc3eb21e2c038f3d0ae99e9d05efa
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-03-16 09:30:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,15 @@ import footballAPI
 #   Teams
 # ===============================================================
 
-parameters_dictionary = {"API type": "teams", "country": "england", "league": "league-one", "end year": None}
+parameters_dictionary = {"API type": "teams", "country": "england", "league": "league-one", "end year": 2019}
 
 client = footballAPI.FootballAPI()
 client.set_parameters(parameters_dictionary)
+
+print("===============")
 print(client.json_data)
+print("===============")
+
 
 # ===============================================================
 #   Leagues
