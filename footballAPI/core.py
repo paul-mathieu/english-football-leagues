1d87e8240095c0f30bb03bc70a49f6b22632bda8
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-05-11 11:03:55 +0200

    add HEADERS global var

--- footballAPI/core.py
+++ footballAPI/core.py
@@ -20,6 +20,9 @@ LEAGUES_END_URL = "/regular-season"
 
 THIS_YEAR = int(str(datetime.datetime.now())[:4])
 
+HEADERS = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, '
+                         'like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+
 
 def get_HTML(url, convert_to_json=False):
     html_content = requests.get(url).text
