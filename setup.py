8f97676ce04cf484e0584e0b488e5fd675cd61b2
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2020-06-03 15:47:49 +0200

    change type parameters

--- setup.py
+++ setup.py
@@ -24,7 +24,11 @@ import footballAPI
 #   Teams
 # ===============================================================
 
-parameters_dictionary = {"API-type": "teams", "country": "england", "league": "league-one", "end year": 2019}
+parameters_dictionary = {"API-type": "teams",
+                         "name-team": "Liverpool", "max-result": 1,
+                         "info": False, "venue": False, "trophies": False,
+                         "matches": False, "squad": True, "fan-sites": False,
+                         "squad-info": {"API-type": "players", 'all': 1}}
 
 client = footballAPI.FootballAPI()
 client.set_parameters(parameters_dictionary)
